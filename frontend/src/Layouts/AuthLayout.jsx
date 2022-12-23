94ea9e313448c3faa8219f23735fdbfc70c02daa
Michael Canales <CDLSMichael.16@gmail.com>
2022-12-23 17:50:55 -0500

    solucionando parentesis

--- frontend/src/Layouts/AuthLayout.jsx
+++ frontend/src/Layouts/AuthLayout.jsx
@@ -6,13 +6,11 @@ const AuthPage = () => {
 
   return (
     <>
-      (
       <main className="container mx-auto mt-5 md:mt-20 p-5 md:flex md:justify-center">
         <div className="md:w-2/3 lg:w-2/5">
           <Outlet />
         </div>
       </main>
-      )
     </>
   );
 };
